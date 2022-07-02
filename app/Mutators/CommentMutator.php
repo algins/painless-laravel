805cc33678d614b10e59edd7678c51569aabb884
algins <igors.algins@gmail.com>
2022-07-02 10:31:10 +0300

    Wrap comment creation in transaction

--- app/Mutators/CommentMutator.php
+++ app/Mutators/CommentMutator.php
@@ -4,16 +4,19 @@ namespace App\Mutators;
 
 use App\Models\Article;
 use App\Models\Article\Comment;
+use Illuminate\Support\Facades\DB;
 
 class CommentMutator
 {
     public static function create(array $attributes, Article $article): Comment
     {
-        $article->increment('comments_count');
-
         $comment = new Comment($attributes);
-        $comment->article()->associate($article);
-        $comment->save();
+
+        DB::transaction(function () use ($comment, $article) {
+            $article->increment('comments_count');
+            $comment->article()->associate($article);
+            $comment->save();
+        });
 
         return $comment;
     }
